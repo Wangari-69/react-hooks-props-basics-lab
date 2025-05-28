8ffc4a9790ab3522b87334862c9646690b1d5cfd
Faith Wangari <njihiafaith@gmail.com>
2025-05-28 12:08:44 +0300

    Adds to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
 console.log(user);
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About name={user.name} bio={user.bio} links={user.links} />
     </div>
   );
 }
